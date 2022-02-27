a58544a8f8e0b42e0d0ca4d0788f6040801ad487
P. Taylor Goetz <ptgoetz@apache.org>
2022-02-27 00:22:47 -0500

    terminal/ansi-terminal detection

--- pkg/ansi/util/util.go
+++ pkg/ansi/util/util.go
@@ -2,19 +2,25 @@ package util
 
 import (
 	"golang.org/x/sys/unix"
+	"os"
 	"syscall"
 )
 
-// IsTTY detects if the current context is a TTY by calling GetTTYDimensions.
+func IsTerminal() bool {
+	fileInfo, _ := os.Stdout.Stat()
+	return (fileInfo.Mode() & os.ModeCharDevice) != 0
+}
+
+// IsAnsiTerminal detects if the current context is a TTY by calling GetTerminalDimensions.
 // If an error is returned, or either rows or cols is zero, it's assumed that it's not a TTY.
-func IsTTY() bool {
-	rows, cols, err := GetTTYDimensions()
+func IsAnsiTerminal() bool {
+	rows, cols, err := GetTerminalDimensions()
 	return err == nil && rows != 0 && cols != 0
 }
 
-// GetTTYDimensions returns the number of rows, and columns of the current TTY, or an error if the current
+// GetTerminalDimensions returns the number of rows, and columns of the current TTY, or an error if the current
 // context is not a TTY.
-func GetTTYDimensions() (rows uint16, cols uint16, err error) {
+func GetTerminalDimensions() (rows uint16, cols uint16, err error) {
 	ws, err := unix.IoctlGetWinsize(syscall.Stdout, unix.TIOCGWINSZ)
 	if err != nil {
 		return 0, 0, err
