f6086d0ed10f3d1976aafdb822211fbc5f664650
P. Taylor Goetz <ptgoetz@apache.org>
2022-03-01 20:05:27 -0500

    add additional cursor functions

--- pkg/ansi/core.go
+++ pkg/ansi/core.go
@@ -49,6 +49,41 @@ func (w *Writer) Print(s string) {
 	fmt.Fprint(w.ioWriter, s)
 }
 
+// SetCursorPos moves the TTY cursor position to row, col.
+func (w *Writer) SetCursorPos(row int, col int) {
+	fmt.Fprint(w.ioWriter, SetCursorPos(row, col))
+}
+
+// SaveCursor tells the TTY to save the current cursor position.
+func (w *Writer) SaveCursor() {
+	fmt.Fprint(w.ioWriter, SaveCursor())
+}
+
+// RestoreCursor tells the TTY to restore the last saved  cursor position.
+func (w *Writer) RestoreCursor() {
+	fmt.Fprint(w.ioWriter, RestoreCursor())
+}
+
+// ClearScreen tells the TTY to clear the screen.
+func (w *Writer) ClearScreen() {
+	fmt.Fprint(w.ioWriter, ClearScreen())
+}
+
+// ClearScreenRemainder tells the TTY to clear the screen from the cursor position.
+func (w *Writer) ClearScreenRemainder() {
+	fmt.Fprint(w.ioWriter, ClearScreenRemainder())
+}
+
+// ClearLine tells the TTY to clear the current line.
+func (w *Writer) ClearLine() {
+	fmt.Fprint(w.ioWriter, ClearLine())
+}
+
+// ClearLineRemainder tells the TTY to clear the current line from the cursor position.
+func (w *Writer) ClearLineRemainder() {
+	fmt.Fprint(w.ioWriter, ClearLineRemainder())
+}
+
 func intArrayToDelimitedString(a []int, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
@@ -72,6 +107,48 @@ func FormatAndReset(s string, codes ...int) string {
 // MoveCursor moves the TTY cursor `places` points in the given `direction`
 func MoveCursor(direction CursorDirection, places int) string {
 	retval := fmt.Sprintf("%s%d%s", Start, places, direction)
+	return retval
+}
+
+// SaveCursor tells the TTY to save the current cursor position.
+func SaveCursor() string {
+	retval := fmt.Sprintf("%s%s", Start, "s")
+	return retval
+}
+
+// RestoreCursor tells the TTY to restore the last saved  cursor position.
+func RestoreCursor() string {
+	retval := fmt.Sprintf("%s%s", Start, "u")
+	return retval
+}
+
+// ClearScreen tells the TTY to clear the screen.
+func ClearScreen() string {
+	retval := fmt.Sprintf("%s%s", Start, "2J")
+	return retval
+}
+
+// ClearScreenRemainder tells the TTY to clear the screen from the current cursor position.
+func ClearScreenRemainder() string {
+	retval := fmt.Sprintf("%s%s", Start, "J")
+	return retval
+}
+
+// ClearLine tells the TTY to clear the current line.
+func ClearLine() string {
+	retval := fmt.Sprintf("%s%s", Start, "2K")
+	return retval
+}
+
+// ClearLineRemainder tells the TTY to clear the current line from the cursor position on.
+func ClearLineRemainder() string {
+	retval := fmt.Sprintf("%s%s", Start, "K")
+	return retval
+}
+
+// SetCursorPos moves the TTY cursor position to row, col.
+func SetCursorPos(line int, col int) string {
+	retval := fmt.Sprintf("%s%d;%dH", Start, line, col)
 	return retval
 }
 
