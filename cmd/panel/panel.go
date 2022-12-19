969974ed81549b1cd742e5b93d14fb61a7658ae6
P. Taylor Goetz <ptgoetz@apache.org>
2022-12-18 23:43:08 -0500

    add to panel demo

--- cmd/panel/panel.go
+++ cmd/panel/panel.go
@@ -8,20 +8,35 @@ import (
 )
 
 func main() {
-	console := ansi.NewConsole(os.Stdout)
+	c := ansi.NewConsole(os.Stdout)
 
-	panel := component.NewPanelWithLabel("ERROR", " ACCESS DENIED! ", -1, 5)
+	panel := component.NewPanelWithLabel("FATAL", " ACCESS DENIED! ", -1, 5)
 
 	panel.BorderColor = color.Red
-	panel.Render(console)
+	panel.Render(c)
+
+	panel.BorderColor = color.Orange
+	panel.Label = "ERROR"
+	panel.Message = "I'm sorry Dave. I'm afraid I can't do that."
+	panel.Render(c)
 
 	panel.BorderColor = color.Yellow
 	panel.Label = "WARNING"
-	panel.Message = "I'm sorry Dave. I'm afraid I can't do that."
-	panel.Render(console)
+	panel.Message = "The beatings will continue until morale improves."
+	panel.Render(c)
 
-	panel.BorderColor = color.Green
-	panel.Message = "That would be cool."
+	panel.BorderColor = color.CornflowerBlue
+	panel.Message = "It would be cool"
 	panel.Label = "INFO"
-	panel.Render(console)
+	panel.Render(c)
+
+	panel.BorderColor = color.DarkGreen
+	panel.Message = "If we killed that frog"
+	panel.Label = "DEBUG"
+	panel.Render(c)
+
+	panel.BorderColor = color.DimGray
+	panel.Message = "It won't croak again"
+	panel.Label = "TRACE"
+	panel.Render(c)
 }
